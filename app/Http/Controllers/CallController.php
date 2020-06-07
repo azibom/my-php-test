bfa937cefc85c9b7637e8c24fec4295d87954777
cshabanic <mrsh13610@gmail.com>
2020-06-07 15:25:20 +0430

    add controller

--- app/Http/Controllers/CallController.php
+++ app/Http/Controllers/CallController.php
@@ -2,9 +2,40 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\EndOfTheCallRequest;
+use App\Http\Requests\NewCallRequest;
+use App\Http\Requests\NewCallWantRequest;
+use App\Http\Requests\NewEmployeeRequest;
+use App\Repositories\Call\CallRepositoryInterface;
 
 class CallController extends Controller
 {
-    //
+    public function __construct(CallRepositoryInterface $callRepository)
+    {
+        $this->callRepository = $callRepository;
+    }
+
+    public function newCall(NewCallRequest $request)
+    {
+        $response = $this->callRepository->newCall($request);
+        return response()->json($response["data"], $response["statusCode"]);
+    }
+
+    public function endOfTheCall(EndOfTheCallRequest $request)
+    {
+        $response = $this->callRepository->endOfTheCall($request);
+        return response()->json($response["data"], $response["statusCode"]);
+    }
+
+    public function newCallWant(NewCallWantRequest $request)
+    {
+        $response = $this->callRepository->newCallWant($request);
+        return response()->json($response["data"], $response["statusCode"]);
+    }
+
+    public function newEmployee(NewEmployeeRequest $request)
+    {
+        $response = $this->callRepository->newEmployee($request);
+        return response()->json($response["data"], $response["statusCode"]);
+    }
 }
