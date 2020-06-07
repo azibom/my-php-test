e85d62967b6159aebd5f6c2a5c66ce4effeef042
cshabanic <mrsh13610@gmail.com>
2020-06-07 15:24:25 +0430

    add models

--- app/Call.php
+++ app/Call.php
@@ -6,5 +6,35 @@ use Illuminate\Database\Eloquent\Model;
 
 class Call extends Model
 {
-    //
+    public function isThereUrgentCall()
+    {
+        $urgentCall = $this->where("state", "waiting")->where("priority", "high")->get();
+        if (count($urgentCall) > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function getUrgentCall()
+    {
+        $urgentCall = $this->where("state", "waiting")->where("priority", "high")->first();
+        return $urgentCall;
+    }
+
+    public function isThereNormalCall()
+    {
+        $urgentCall = $this->where("state", "waiting")->where("priority", "low")->get();
+        if (count($urgentCall) > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function getNormalCall()
+    {
+        $urgentCall = $this->where("state", "waiting")->where("priority", "low")->first();
+        return $urgentCall;
+    }
 }
--- app/Employee.php
+++ app/Employee.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
-    //
+    public function isThereFreeEmployee()
+    {
+        $freeEmployees = $this->where("state", "waiting")->get();
+        if (count($freeEmployees) > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function getFreeEmployee()
+    {
+        $freeEmployee = $this->where("state", "waiting")->orderBy('priority', 'desc')->first();
+        return $freeEmployee;
+    }
 }
