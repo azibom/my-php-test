ba9a1e432c230132b244b1dd53efe827f906eb18
cshabanic <mrsh13610@gmail.com>
2020-06-07 15:25:48 +0430

    add service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Repositories\Call\CallRepository;
+use App\Repositories\Call\CallRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(CallRepositoryInterface::class, CallRepository::class);
     }
 
     /**
