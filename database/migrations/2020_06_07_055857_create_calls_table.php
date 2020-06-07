f40966b06ccd51e765fbdc015f7d74bb6e73371c
cshabanic <mrsh13610@gmail.com>
2020-06-07 10:52:10 +0430

    add models

---
+++ database/migrations/2020_06_07_055857_create_calls_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCallsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('calls', function (Blueprint $table) {
+            $table->id();
+            $table->string('hash');
+            $table->enum('priority', ['high', 'low']);
+            $table->enum('state', ['waiting', 'doing', 'done']);
+            $table->unsignedBigInteger('responsible_id');
+            $table->foreign('responsible_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('calls');
+    }
+}
